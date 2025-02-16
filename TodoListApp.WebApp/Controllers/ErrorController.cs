c2aa107d5b86ab6641a15ac658e048f87fa34559
Kamil Kondziolka <wono83@interia.pl>
2025-02-16 19:58:43 +0100

    minor fixes

--- TodoListApp.WebApp/Controllers/ErrorController.cs
+++ TodoListApp.WebApp/Controllers/ErrorController.cs
@@ -1,23 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace TodoListApp.WebApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 public class ErrorController : Controller
 {
     [Route("Error/NotFound")]
     public IActionResult NotFound()
     {
-        return View();
+        return this.View();
     }
 
     [Route("Error/SessionExpired")]
     public IActionResult SessionExpired()
     {
-        TempData["Error"] = "Session has expired. Please log in again.";
-        return RedirectToAction("Login", "Account");
+        this.TempData["Error"] = "Session has expired. Please log in again.";
+        return this.RedirectToAction("Login", "Auth");
     }
 
-    [Route("Error/Internal")]
-    public IActionResult Internal()
+    [Route("Error/Server")]
+    public IActionResult Server()
     {
-        return View();
+        return this.View();
     }
 }
